d3f18470afa1f8c5a7a74e177d5fc3c7088403ce
Cynthia Dodzi <dahesey@gmail.com>
2024-04-13 22:17:16 +0100

    debugging using strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#Fixing_errors_on_a_wordpress_site_running_Apache
+exec {'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
